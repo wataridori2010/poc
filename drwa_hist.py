d977ac9b3edd359b6b964de92b7c208f8e87ee6f
wataridori2010 <wataridori2010@gmail.com>
2017-04-07 00:59:09 +0900

    コメント

--- drwa_hist.py
+++ drwa_hist.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+# http://www.lifewithpython.com/2014/01/python-flatten-nested-lists.html
 def flattenf(nested_list):
     """2重のリストをフラットにする関数"""
     return [e for inner_list in nested_list for e in inner_list]
